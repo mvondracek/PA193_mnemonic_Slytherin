3668d9f93b24a1f1dbc350c6f7c8a39ec5f87201
Martin Vondráček <vondracek.mar@gmail.com>
2019-10-14 23:18:08 +0200

    cli: tests: execute cli with absolute path

--- PA193_mnemonic_Slytherin/test_mnemoniccli.py
+++ PA193_mnemonic_Slytherin/test_mnemoniccli.py
@@ -22,16 +22,17 @@ class TestMain(unittest.TestCase):
     def setUp(self):
         self.timeout = 5  # seconds until we terminate the program
         self.cli_dir = os.path.dirname(os.path.abspath(__file__))
+        self.cli_path = os.path.join(self.cli_dir, 'mnemoniccli.py')
 
     def assert_argument_error(self, args):
-        cli = subprocess.run(args, cwd=self.cli_dir, timeout=self.timeout, shell=True,
+        cli = subprocess.run(args, timeout=self.timeout, shell=True,
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         self.assertEqual('', cli.stdout)
         self.assertNotEqual('', cli.stderr)
         self.assertEqual(ExitCode.ARGUMENTS.value, cli.returncode)
 
     def assert_argument_ok_terminated(self, args):
-        cli = subprocess.run(args, cwd=self.cli_dir, timeout=self.timeout, shell=True,
+        cli = subprocess.run(args, timeout=self.timeout, shell=True,
                              stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         self.assertNotEqual('', cli.stdout)
         self.assertEqual('', cli.stderr)
@@ -39,32 +40,32 @@ class TestMain(unittest.TestCase):
 
     def test_arguments_error(self):
         """invalid arguments"""
-        self.assert_argument_error(['mnemoniccli.py'])
-        self.assert_argument_error(['mnemoniccli.py', '-ll', 'FOO'])
-        self.assert_argument_error(['mnemoniccli.py', '-g'])
-        self.assert_argument_error(['mnemoniccli.py', '-r'])
-        self.assert_argument_error(['mnemoniccli.py', '-v'])
-        self.assert_argument_error(['mnemoniccli.py', '-g', '-r', '-v'])
+        self.assert_argument_error([self.cli_path])
+        self.assert_argument_error([self.cli_path, '-ll', 'FOO'])
+        self.assert_argument_error([self.cli_path, '-g'])
+        self.assert_argument_error([self.cli_path, '-r'])
+        self.assert_argument_error([self.cli_path, '-v'])
+        self.assert_argument_error([self.cli_path, '-g', '-r', '-v'])
 
     def test_arguments_ok_terminated(self):
         """correct argument resulting in termination"""
-        self.assert_argument_ok_terminated(['mnemoniccli.py', '-h'])
-        self.assert_argument_ok_terminated(['mnemoniccli.py', '--help'])
-        self.assert_argument_ok_terminated(['mnemoniccli.py', '-V'])
-        self.assert_argument_ok_terminated(['mnemoniccli.py', '--version'])
+        self.assert_argument_ok_terminated([self.cli_path, '-h'])
+        self.assert_argument_ok_terminated([self.cli_path, '--help'])
+        self.assert_argument_ok_terminated([self.cli_path, '-V'])
+        self.assert_argument_ok_terminated([self.cli_path, '--version'])
 
     def test_arguments_error_file_path(self):
         """input files don't exist"""
         with TemporaryDirectory() as tmpdir:
             non_existing_filepath = os.path.join(tmpdir, '__this_file_does_not_exist__')
-            self.assert_argument_error(['mnemoniccli.py', '-g', '-e', non_existing_filepath])
-            self.assert_argument_error(['mnemoniccli.py', '-r', '-m', non_existing_filepath])
-            self.assert_argument_error(['mnemoniccli.py', '-v', '-m', non_existing_filepath, '-s', non_existing_filepath])
+            self.assert_argument_error([self.cli_path, '-g', '-e', non_existing_filepath])
+            self.assert_argument_error([self.cli_path, '-r', '-m', non_existing_filepath])
+            self.assert_argument_error([self.cli_path, '-v', '-m', non_existing_filepath, '-s', non_existing_filepath])
 
             with open(os.path.join(tmpdir, '__this_file_exists__.txt'), 'w') as f:
                 f.write('foo bar')
-            self.assert_argument_error(['mnemoniccli.py', '-v', '-m', f.name, '-s', non_existing_filepath])
-            self.assert_argument_error(['mnemoniccli.py', '-v', '-m', non_existing_filepath, '-s', f.name])
+            self.assert_argument_error([self.cli_path, '-v', '-m', f.name, '-s', non_existing_filepath])
+            self.assert_argument_error([self.cli_path, '-v', '-m', non_existing_filepath, '-s', f.name])
 
     def test_invalid_entropy(self):
         """Invalid input file with entropy
@@ -84,8 +85,8 @@ class TestMain(unittest.TestCase):
                 with self.subTest(entropy_bytes_length=entropy_bytes_length):
                     with open(os.path.join(tmpdir, '__entropy_binary__.dat'), 'wb') as f:
                         f.write(entropy_byte * entropy_bytes_length)
-                    cli = subprocess.run(['mnemoniccli.py', '-g', '-e', f.name, '--format', 'bin'],
-                                         cwd=self.cli_dir, timeout=self.timeout, shell=True,
+                    cli = subprocess.run([self.cli_path, '-g', '-e', f.name, '--format', 'bin'],
+                                         timeout=self.timeout, shell=True,
                                          stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
                     self.assertEqual('', cli.stdout)
                     self.assertEqual('invalid entropy\n', cli.stderr)
