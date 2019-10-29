51129793d4a317e0be7e0c5464f6eea68adf51fc
Martin Vondráček <vondracek.mar@gmail.com>
2019-10-29 22:23:41 +0100

    tests: use unittest runner

--- setup.py
+++ setup.py
@@ -39,10 +39,8 @@ setup(
         'pa193mnemonicslytherin': ['*.lst'],
     },
     tests_require=[
-        'nose',
         'coverage',
     ],
-    test_suite='nose.collector',
     entry_points={
         'console_scripts': [
             'mnemoniccli = pa193mnemonicslytherin.mnemoniccli:cli_entry_point',
