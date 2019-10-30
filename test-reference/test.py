53c648a19bc7c869ba1522ff4d37499809666624
Samuel Obuch <samuel.obuch17@gmail.com>
2019-10-30 11:45:52 +0100

    style

--- test-reference/test.py
+++ test-reference/test.py
@@ -8,13 +8,16 @@ ref_Mnemonic = reference_Mnemonic("english")
 while True:
     password = ""
     test_mnemonic = ref_Mnemonic.generate()
-    test_entropy =  bytes(ref_Mnemonic.to_entropy(test_mnemonic))
+    test_entropy = bytes(ref_Mnemonic.to_entropy(test_mnemonic))
     test_seed = bytes(ref_Mnemonic.to_seed(test_mnemonic, password))
     test_mnemonic2 = ref_Mnemonic.to_mnemonic(test_entropy)
-    
+
     if test_entropy != Mnemonic(test_mnemonic).to_entropy():
         print("difference for conversion to entropy for '{}'".format(test_mnemonic))
-    if test_seed != bytes(Mnemonic(test_mnemonic).to_seed(password)): # cant use secure seed compare here hence the conversion to bytes
+
+    # cant use secure seed compare here hence the conversion to bytes
+    if test_seed != bytes(Mnemonic(test_mnemonic).to_seed(password)):
         print("difference for conversion to seed for '{}' and '{}'".format(test_mnemonic, password))
+
     if test_mnemonic2 != Entropy(test_entropy).to_mnemonic():
         print("difference for conversion to mnemonic for '{}'".format(test_mnemonic))
