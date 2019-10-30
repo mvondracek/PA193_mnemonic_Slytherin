24f45e0446b89b90e4bdd66995d58e0286c10d74
Martin Vondráček <vondracek.mar@gmail.com>
2019-10-30 22:28:42 +0100

    test-reference/install.sh: pip global

--- test-reference/install.sh
+++ test-reference/install.sh
@@ -2,4 +2,4 @@
 set -Eeuo pipefail
 git clone https://github.com/trezor/python-mnemonic.git
 cd python-mnemonic
-pip install . --user # TODO it would be even better to use venv
+pip install . # it is better to use venv
