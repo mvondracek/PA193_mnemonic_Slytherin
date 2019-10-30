92251aecf0f2879359161620cd923b496814dcb6
Martin Vondráček <vondracek.mar@gmail.com>
2019-10-30 20:52:49 +0100

    test-reference/install.sh: set error checks

--- test-reference/install.sh
+++ test-reference/install.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-git clone https://github.com/trezor/python-mnemonic.git && cd python-mnemonic && sudo python setup.py install && cd ..
+#!/usr/bin/env bash
+set -Eeuo pipefail
+git clone https://github.com/trezor/python-mnemonic.git
+cd python-mnemonic
+sudo python setup.py install
 
